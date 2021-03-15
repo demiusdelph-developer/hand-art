21b68c8c6f295674ff12bf965a7f8a7c2544b7be
Dmutro <demius_delph@hotmail.com>
2021-03-15 05:17:58 +0200

    test

---
+++ config/config.local.php
@@ -0,0 +1,16 @@
+;<? exit(); ?>
+ 
+[database]
+ 
+;Сервер базы данных
+db_server = localhost
+ 
+;Пользователь базы данных
+db_user = handart
+ 
+;Пароль к базе
+db_password = "qY7LZCZT"
+ 
+;Имя базы
+db_name = handart
+ 
